b7dd7bf359b87f547118a80ef0e6b96b41bd935c
JahirPendhari <jahirpp1999@gmail.com>
2024-04-04 23:46:51 +0530

    images rendering fixed

--- frontend/src/components/MainContainer.tsx
+++ frontend/src/components/MainContainer.tsx
@@ -1,21 +1,23 @@
 import React from 'react'
 
 type props ={
-  currCustomer :number
+  currCustomer :number;
+  images :string[]
 }
-const MainContainer:React.FC<props> = ({currCustomer}) => {
+const MainContainer:React.FC<props> = ({currCustomer, images}) => {
+
   return (
     <div className='mx-10 '>
       <h1 className='text-center text-2xl mt-4'>Customer {currCustomer+1} Details here</h1>
       <p className='text-center mt-5 w-10/12 m-auto'>{currCustomer%2 ==0 ? 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias numquam, sed est repudiandae amet molestias perferendis optio sint velit repellat ipsam explicabo! Quae animi temporibus et voluptates laudantium, velit architecto ad enim quia natus ab, dolorum nam eveniet, error aliquid.':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aspernatur, autem voluptas quibusdam sunt odit vel sint porro omnis, recusandae reiciendis fugit eos nostrum eveniet repellendus ad rerum cumque delectus!'}</p>
-      <div className=' grid grid-cols-3 w-11/12 m-auto gap-4 mt-4' >
-        {
-          new Array(9).fill(1).map((curr)=>{
-            return <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlUInaG1-Xp2oYbAy2FN57lRqQ0crQ7VdNxJjFeh_adw&s" alt="random-image" className='rounded-xl cursor-pointer'/>
-          })
-        }
-        
-      </div>
+      <div className='grid grid-cols-3 gap-4 transition-all mt-4 duration-1000 ease-in-out'>
+      {images.map((image, index) => (
+        <div key={index}>
+          <img src={image} alt={`Image ${index}`} className='rounded-lg shadow-md w-7/12 m-auto h-[120px] p-4 hover:scale-105 transition-transform duration-1000 ease-in-out' />
+        </div>
+      ))}
+    </div>
+
     </div>
   )
 }
